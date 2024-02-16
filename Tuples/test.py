6a060b0cf4c2492f1948c4f6eb0fd8df6d505308
squalovarun <varunkulshrestha12345@gmail.com>
2024-02-16 15:27:05 +0530

    excel push

--- Tuples/test.py
+++ Tuples/test.py
@@ -4,7 +4,13 @@
 # print(T[1][1])
 # print(T.pop)
 
+# T = (1,2,3,4,5,6,7,8,9)
+#
+# [T[i]for i in range(0, len(T),2) ]
+# print(T[:])
+
 T = (1,2,3,4,5,6,7,8,9)
 
 [T[i]for i in range(0, len(T),2) ]
-print(T[:])
+
+print(T)
